9b9948d1ab321eb49fb905599d234eca161f62ab
sergio5535 <sergionathaniel5535@gmail.com>
2021-11-09 20:32:18 +0700

    update font navbar

--- gejala.php
+++ gejala.php
@@ -10,9 +10,11 @@ $result = mysqli_query($conn, "SELECT * FROM gejala");
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;1,200&display=swap" rel="stylesheet">
     <title>Gejala Page</title>
-    <?php include_once 'navbar.php' ?>
     <link href="css/gejala.css" rel="stylesheet">
+    <link href="css/allpage.css" rel="stylesheet">
+    <?php include_once 'navbar.php' ?>
 </head>
 
 <body>
--- index.php
+++ index.php
@@ -11,6 +11,7 @@
     <link href="css/index.css" rel="stylesheet">
     <link href="css/navbar.css" rel="stylesheet">
     <link href="css/konsul.css" rel="stylesheet">
+    <link href="css/allpage.css" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;1,200&display=swap" rel="stylesheet">
