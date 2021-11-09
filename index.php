da54a3a1bfd834e5b525d8317319a80beca26a52
jryarianto <jerry.arianto@student.umn.ac.id>
2021-11-09 16:29:23 +0700

    create index.php

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    <link href="css/index.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Permanent+Marker&display=swap" rel="stylesheet">
+    <title>DSION</title> -->
+</head>
+
+<body>
+    <!-- <?php include 'Navbar.php'; ?> -->
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-5">
+                <!-- <img src="images/Depression 1.svg" width="110%"> -->
+            </div>
+            <div class="col-md-7">
+                <h1>TES PARAMETER DEPRESI ANDA<img src="images/MoonStar.png"></h1>
+                <a href="pertanyaan.php">
+                    <img src="images/PlayButton.png" id="play">
+                </a>
+                <a href="pertanyaan.php">
+                    <button id="btn-solusi">Mulai Tes</button>
+                </a>
+            </div>
+        </div>
+    </div>
+    <img src="images/plant.png" id="plant">
+</body>
+
+</html>
