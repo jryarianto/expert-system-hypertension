d95620a67e2a2e5c3b4d06bc7bd2267f082f85b7
jryarianto <jerry.arianto@student.umn.ac.id>
2021-11-09 19:59:30 +0700

    revisi index.php

--- index.php
+++ index.php
@@ -8,18 +8,9 @@
     <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"> -->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-<<<<<<< HEAD
     <link href="css/index.css" rel="stylesheet">
-=======
     <link href="css/navbar.css" rel="stylesheet">
     <link href="css/konsul.css" rel="stylesheet">
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> ecc7b9c6bb6d8076512bfa849586b1a4c6e7602a
-=======
->>>>>>> ecc7b9c6bb6d8076512bfa849586b1a4c6e7602a
-=======
->>>>>>> ecc7b9c6bb6d8076512bfa849586b1a4c6e7602a
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;1,200&display=swap" rel="stylesheet">
@@ -28,23 +19,6 @@
 
 <body>
     <?php include 'navbar.php'; ?>
-    <!-- <div class="container mt-5">
-        <div class="row">
-            <div class="col-md-5">
-                <img src="images/Depression 1.svg" width="110%">
-            </div>
-            <div class="col-md-7">
-                <h1>TES PARAMETER DEPRESI ANDA<img src="images/MoonStar.png"></h1>
-                <a h`ref="pertanyaan.php">
-                    <img src="images/PlayButton.png" id="play">
-                </a>
-                <a href="konsul.php" class="button button1">
-                    <button id="btn-solusi">Mulai Tes</button>
-                </a>
-            </div>
-        </div>
-    </div>
-    <img src="images/plant.png" id="plant"> -->
 
     <div class="container1">
 
