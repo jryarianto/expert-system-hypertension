db9ca857dfd7c5c1a23e2b39e1f44e5132a4135f
jryarianto <jerry.arianto@student.umn.ac.id>
2021-11-10 16:17:29 +0700

    gejala.php kelar

--- gejala.php
+++ gejala.php
@@ -7,52 +7,50 @@ $result = mysqli_query($conn, "SELECT * FROM gejala");
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;1,200&display=swap" rel="stylesheet">
     <title>Gejala Page</title>
     <link href="css/gejala.css" rel="stylesheet">
-    <link href="css/allpage.css" rel="stylesheet">
-    <?php include_once 'navbar.php' ?>
 </head>
 
 <body>
-    <h1>Gejala</h1>
-    <h3>Sistem Pakar Diagnosa Penyakit Hipertensi</h3>
+    <?php include 'navbar.php' ?>
     <h2 class="title">Tabel Gejala</h2>
-    <div class="bg1">
-        <di class="bg2">
-            <?php
-            if (!$result || mysqli_num_rows($result) > 0) {
-            ?>
-                <table id="customer">
-                    <thead>
+    <!-- <div class="bg1">
+        <div class="bg2"> -->
+    <?php
+    if (!$result || mysqli_num_rows($result) > 0) {
+    ?>
+        <div class="tablebox">
+            <table id="customer">
+                <thead>
+                    <tr>
+                        <td style="text-align: center;">Nomor Gejela</td>
+                        <td style="text-align: center;">Nama Gejala</td>
+                    </tr>
+                </thead>
+                <?php
+                while ($row = mysqli_fetch_array($result)) {
+                ?>
+                    <tbody>
                         <tr>
-                            <td style="text-align: center;">Nomor Gejela</td>
-                            <td style="text-align: center;">Nama Gejala</td>
+                            <td style="text-align: center;"><?php echo $row["id_gejala"]; ?></td>
+                            <td style="text-align: center;"><?php echo $row["gejala"]; ?></td>
                         </tr>
-                    </thead>
-                    <?php
-                    while ($row = mysqli_fetch_array($result)) {
-                    ?>
-                        <tbody>
-                            <tr>
-                                <td style="text-align: center;"><?php echo $row["id_gejala"]; ?></td>
-                                <td><?php echo $row["gejala"]; ?></td>
-                            </tr>
-                        </tbody>
-                    <?php
-                    }
-                    ?>
-                </table>
-            <?php
-            } else {
-                echo "No result found";
-            }
-            ?>
-        </di>
-    </div>
+                    </tbody>
+                <?php
+                }
+                ?>
+            </table>
+        </div>
+    <?php
+    } else {
+        echo "No result found";
+    }
+    ?>
+    <!-- </div>
+    </div> -->
 </body>
 
 </html>
