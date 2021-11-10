3b6a1845c3d0c96ecf3818ec0be74e249a5ef65d
sergio5535 <sergionathaniel5535@gmail.com>
2021-11-10 16:17:53 +0700

    minor

--- hasil.php
+++ hasil.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Result Page</title>
+    <link href="css/hasil.css" rel="stylesheet">
     <?php include_once 'navbar.php' ?>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;1,200&display=swap" rel="stylesheet">
 </head>
