53cd0fc531fae32149ac345e7f81274b69853ea8
sergio5535 <sergionathaniel5535@gmail.com>
2021-11-22 19:09:37 +0700

    css update again

--- solusi.php
+++ solusi.php
@@ -13,6 +13,7 @@ $crud = new Crud();
 
 <head>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;1,200&display=swap" rel="stylesheet">
     <link href="css/solusi.css" rel="stylesheet">
     <title>Solution Page</title>
 </head>
